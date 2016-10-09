1ad7d6431764805fee903e026d449cbfc6fb9916
Mindrik_EV <banzay_08@mail.ru>
2016-10-09 17:48:19 +0300

    task3_2 (Variant 1) Generate three-digit number and print max digit

---
+++ task3/task3_2/src/task3_2/Main.java
@@ -0,0 +1,10 @@
+package task3_2;
+
+public class Main {
+	public static void main(String[] args){
+		Number number = new Number();
+		
+		System.out.println("Generated number is - " + number);  // I used "+" because here is single operation of concatenation.
+		System.out.print("MAX number = " + number.getMax());
+	}
+}
