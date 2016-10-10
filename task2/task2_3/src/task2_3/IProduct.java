084809b11ec85b7ef89d042d498a3c325e8c346c
Mindrik_EV <banzay_08@mail.ru>
2016-10-10 13:02:56 +0300

    ER

--- task2/task2_3/src/task2_3/IProduct.java
+++
@@ -1,7 +0,0 @@
-package task2_3;
-
-public interface IProduct {
-	void installFirstPart(IProductPart productPart);
-	void installSecondPart(IProductPart productPart);
-	void installThirdPart(IProductPart productPart);
-}
