be95fd0691dce5c59b19351f16ddcc538f802d45
Mindrik_EV <banzay_08@mail.ru>
2016-10-10 13:05:12 +0300

    task2_3 VErsion 3

---
+++ task2/task2_3/src/task2_3/LineStep1.java
@@ -0,0 +1,16 @@
+package task2_3;
+
+public class LineStep1 {
+
+	public LineStep1(){
+		System.out.println("LINE STEP >>> 1");
+	}
+	
+//------------------------------------------------------------	
+	
+	public IProductPart buildProductPart() {
+		ProductPartBody pp = new ProductPartBody();
+		
+		return pp;
+	}
+}
