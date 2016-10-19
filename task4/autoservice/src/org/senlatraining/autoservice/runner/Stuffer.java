4980525b9f9dcb3f0b73701a9767a50712d72992
unknown <lelchiknik@mail.ru>
2016-10-20 00:21:44 +0300

    hh

---
+++ task4/autoservice/src/org/senlatraining/autoservice/runner/Stuffer.java
@@ -0,0 +1,9 @@
+package org.senlatraining.autoservice.runner;
+
+import org.senlatraining.autoservice.entity.*;
+import org.senlatraining.autoservice.manager.*;
+import org.senlatraining.autoservice.util.*;
+
+public class Stuffer {
+
+}
---
+++ task4/autoservice/src/org/senlatraining/autoservice/util/sort/Sort.java
@@ -0,0 +1,21 @@
+package org.senlatraining.autoservice.util.sort;
+
+public class Sort {
+
+	import java.util.Comparator;
+	import org.senlatraining.autoservice.entity.*;
+
+	public class CompByNameAndStatus implements Comparator<Master>{
+		
+		//public Comparator<Master> masterComparator = new Comparator<Master>(){
+			@Override
+			public int compare(Master m1, Master m2){
+			int f = m1.getName() - m2.getSurName();
+			if(f == 0){
+				f = m1.getSurName().compareTo(m2.getSurName());
+				return f;
+			}
+		}; 
+	}
+	
+}
