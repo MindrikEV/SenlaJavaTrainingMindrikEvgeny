8bf7632ba261c8870cf819de7e4c99185a797501
Mindrik_EV <banzay_08@mail.ru>
2016-12-02 16:39:54 +0300

    1

--- task6/autoservice + UI/UI/src/org/senlatraining/ui/runner/Runner.java
+++ task6/autoservice + UI/UI/src/org/senlatraining/ui/runner/Runner.java
@@ -1,22 +1,22 @@
 package org.senlatraining.ui.runner;
 
-import org.senlatraining.autoservice.entity.Garage;
+//import org.senlatraining.autoservice.entity.Garage;
 import org.senlatraining.autoservice.frontage.Service;
 import org.senlatraining.ui.control.MenuController;
-import org.senlatraining.ui.util.Printer;
+//import org.senlatraining.ui.util.Printer;
 
 public class Runner {
 public static void main(String[] args){
 		
-	/*	Service ser = null;
-		ser.addNewGarage();
-		ser.addNewGarage();
-		ser.addNewGarage();
-		ser.addNewGarage();
-		ser.addNewGarage();
-		ser.addNewGarage();
-		ser.addNewGarage();
-		ser.addNewGarage();*/
+		Service service = null;
+		service.addNewGarage();
+		service.addNewGarage();
+		service.addNewGarage();
+		service.addNewGarage();
+		service.addNewGarage();
+		service.addNewGarage();
+		service.addNewGarage();
+		service.addNewGarage();
 		
 		MenuController menuController = new MenuController();	
 		menuController.run();
